ebff0f657f785e0343a523cd27deefa274dbc873
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-05-20 18:25:14 +0300

    refactor attend button method

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -6,32 +6,13 @@ module AttendancesHelper
     view_attendees = link_to 'View Attendees', event_attendances_path(event), class: 'book-now'
     attending = link_to 'Attending', '#', class: 'attending-btn'
     if event.creator_id == user.id
-      return view_attendees
-      elsif event.attendees.include?(current_user.username)
-    # elsif (!event.attendees.empty?) && (event.attendees.to_a.any?{ |e| e.id == user.id})
-      return attending
+      view_attendees
+    elsif !event.attendees.empty? && (event.attendees.to_a.any? { |e| e.user == user })
+      attending
     elsif event.attendees.empty?
-      return post_link
+      post_link
     else
-      return post_link
-
-    # if creator == user.id
-    #   view_attendees
-    # elsif !attend.empty?
-    #   attend = attend.to_a
-    #   attend.each do |i|
-    #     if i.user.username.include?(user.username)
-    #       return 
-    #     elsif attend.any?{ |n| n.attendee.username == user.username}
-    #     # elsif i.user.username.include?(user.username)
-    #       return view_attendees
-    #     else
-    #       # return "#{user.username}, #{i.user.username}"
-    #       return post_link
-    #     end
-    #   end
-    # else
-    #   post_link
+      post_link
     end
   end
 end
