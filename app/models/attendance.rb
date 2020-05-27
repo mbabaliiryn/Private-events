8ce289d177dded77edf355481e7bfcd819f2c512
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-05-27 10:11:26 +0300

    revise relations

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,5 +1,4 @@
 class Attendance < ApplicationRecord
-  # belongs_to :event
   has_many :attendees, foreign_key: :attendee_id,class_name: 'User'
   belongs_to :users, class_name: 'User', foreign_key: 'attendee_id'
   belongs_to :event, class_name: 'Event', foreign_key: 'event_id'
